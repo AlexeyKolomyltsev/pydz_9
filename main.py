fbf9db73843359f86f9d765b1972836c32bb53f6
Alexey_wl <allex_doma@mail.ru>
2023-01-05 18:39:31 +0800

    добавил загрузку аудио

--- main.py
+++ main.py
@@ -27,38 +27,32 @@ async def user_message(message:types.Message):
     if message.text.startswith == 'https://www.youtube.com/' or 'https://youtu.be/':
         message_text = f'Загрузка видео {yt.title} \nС канала {yt.author} будет начата после выбора качества'
         await bot.send_message(chat_id, message_text, reply_markup = make_keyboards(url))
-        #await download_video(url, message, bot)
+ 
 
 
 @dp.callback_query_handler()
 async def handler_call(call: types.CallbackQuery):
     chat_id = call.from_user.id
+    url = get_url(call.data)
+    yt = YouTube(url)
+
     if call.data.startswith('low'):
-        url = get_url(call.data)
-        yt = YouTube(url)
         stream = yt.streams.filter(progressive=True, file_extension='mp4')
         stream.get_lowest_resolution().download(f'{chat_id}', f'{chat_id}_{yt.title}')
         
     elif call.data.startswith('hight'):
-        url = get_url(call.data)
-        yt = YouTube(url)
         stream = yt.streams.filter(progressive=True, file_extension='mp4')
         stream.get_highest_resolution().download(f'{chat_id}', f'{chat_id}_{yt.title}')
-        
+
+    elif call.data.startswith('audio'):
+        stream = yt.streams.filter(only_audio=True).first()
+        stream.download(f'{chat_id}', f'{chat_id}_{yt.title}')
+
     path = f'{chat_id}' + os.sep + f'{chat_id}_{yt.title}'    
     with open(path, 'rb') as video:
         await bot.send_video(chat_id, video, caption="Загрузка завершена")
         os.remove(path)
 
-# async def download_video(url, message, bot):
-#     yt = YouTube(url)
-#     stream = yt.streams.filter(progressive=True, file_extension='mp4')
-#     stream.get_lowest_resolution().download(f'{message.chat.id}', f'{message.chat.id}_{yt.title}')
-#     path = f'{message.chat.id}' + os.sep + f'{message.chat.id}_{yt.title}'
-#     print(path)
-#     with open(path, 'rb') as video:
-#         await bot.send_video(message.chat.id, video, caption="Загрузка завершена")
-#         os.remove(path)
 
 if __name__ == '__main__':
     executor.start_polling(dp)
